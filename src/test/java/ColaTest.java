ade517f8b40501654301a052088d97ac0f57498a
yehorl <yehorl@sogtwareplanet.uk.com>
2017-06-13 13:43:34 +0300

    Add not compiling test

--- src/test/java/ColaTest.java
+++ src/test/java/ColaTest.java
@@ -1,5 +1,16 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+
 public class ColaTest {
 
+    @Test
+    public void countPriceReturnsCorrectPriceWhenPriceForMillilitreIsGiven(){
+    double expected_price = 1300;
+
+    Cola cola = new Cola(1000);
+    cola.price = cola.countPrice(1.3);
+
+    assert(expected_price == cola.price);
+    }
 }
